2bb90d97a32d2dc4603dac12f34a97cf563a5279
Josh B <jrb7183@psu.edu>
2025-07-31 20:11:12 -0400

    Added Phonology Inputs to App

--- relangapp/src/App.js
+++ relangapp/src/App.js
@@ -7,27 +7,71 @@ import InputTable from "./components/inputTable";
 // import getUser from './apiCalls';
 
 function App() {
-  const [rows, setRows] = useState([]);
+  const [outRows, setRows] = useState([]);
 
-  const [input, setInput] = useState("");
+  const [consNum, setConsNum] = useState("");
+  const [numPhonos, setNumPhonos] = useState(1);
+
+  let initInputs = new Array(8).fill(new Array(11).fill(""));
+  const [inputs, setInputs] = useState([initInputs]);
+
+  const handleInputs = (event, phonoNum, row, column) => {
+    const newInputs = inputs.map((phono, k) => {
+      
+      if (k === phonoNum) {
+        return phono.map((rowlist, i) => {
+
+          if (i === row) {
+            return rowlist.map((val, j) => {
+
+              if (j === column) {
+                return event.target.value;
+              }
+              return val;
+            });
+          }
+          return rowlist;
+        });
+      }
+      return phono;
+    });
+
+    setInputs(newInputs);
+  }
 
   const handleChange = (event) => {
-    setInput(event.target.value);
+    setConsNum(event.target.value);
   }
 
   const newCons = async (num) => {
-    const new_rows = await requestCons(input); 
+    const new_rows = await requestCons(consNum); 
     setRows(new_rows);
+  }
+
+  const addPhono = (event) => {
+    setNumPhonos(numPhonos + 1);
+    const newInputs = inputs.concat([new Array(8).fill(new Array(11).fill(""))]);
+    setInputs(newInputs);
+  }
+
+  const removePhono = (event) => {
+    if (numPhonos > 1) {
+      setNumPhonos(numPhonos - 1);
+      const newInputs = inputs.filter((_, i) => i !== numPhonos - 1);
+      setInputs(newInputs);
+    }
   }
 
   return (
     <div className="App">
       <header className="App-header">
-        <InputTable index={0}/>
-        <ConsTable rows={rows} />
+        <button type="button" onClick={addPhono} >Add Phonology</button>
+        {numPhonos > 1 ? <button type="button" onClick={removePhono} >Remove Phonology</button> : <></>}
+        {Array.from({length: numPhonos}).map((_, i) => <InputTable key={i} index={i} inputs={inputs[i]} handleInputs={(event, row, column) => handleInputs(event, i, row, column)} />)}
+        <ConsTable rows={outRows} />
         <form name="Test Form">
-            <label>Enter Number of Consonants Here: <input name="Num Cons" type="text" value={input} onChange={handleChange}/></label>
-            <button type="button" onClick={() => {newCons(input)}} >Get Consonants!</button>
+            <label>Enter Number of Consonants Here: <input name="Num Cons" type="text" value={consNum} onChange={handleChange}/></label>
+            <button type="button" onClick={() => {newCons(consNum)}} >Get Consonants!</button>
         </form>
       </header>
     </div>
--- relangapp/src/components/inputTable.js
+++ relangapp/src/components/inputTable.js
@@ -1,5 +1,3 @@
-import { useState } from 'react';
-
 import './inputTable.css';
 
 function Cell(props) {
@@ -14,7 +12,6 @@ function Cell(props) {
 function Row(props) {
     const places = ["Bilabial", "Labiodental", "Dental", "Alveolar", "Retroflex", "Post-Alveolar", "Palatal", "Velar", "Uvular", "Pharyngeal", "Glottal"];
 
-    console.log(props.isHeader);
     if (props.isHeader) {
         return (
             <tr className="inputTable" >
@@ -23,7 +20,7 @@ function Row(props) {
             </tr>
         );
     }
-    console.log("butts");
+
     return(
         <tr className="inputTable" >
             <th className="inputTable" key={0}> {props.manner} </th>
@@ -34,24 +31,6 @@ function Row(props) {
 
 function InputTable(props) {
     const manners = ["Nasal", "Plosive", "Affricate", "Fricative", "Sibilant", "Trill", "Tap", "Approximant"];
-    let initInputs = new Array(8).fill(new Array(11).fill(""));
-    const [inputs, setInput] = useState(initInputs);
-
-    const handleInput = (event, row, column) => {
-        const newInputs = inputs.map((rowlist, i) => {
-            if (i === row) {
-                return rowlist.map((val, j) => {
-
-                    if (j === column) {
-                        return event.target.value;
-                    }
-                    return val;
-                });
-            }
-            return rowlist;
-        });
-        setInput(newInputs);
-    }
 
     return (
         <>
@@ -59,7 +38,7 @@ function InputTable(props) {
         <table className="inputTable">
             <tbody>
                 <Row key={0} isHeader={true} />
-                {manners.map((manner, i)=> <Row key={i + 1} isHeader={false} manner={manner} inputs={inputs[i]} handler={(event, column) => handleInput(event, i, column)}/>)}
+                {manners.map((manner, i)=> <Row key={i + 1} isHeader={false} manner={manner} inputs={props.inputs[i]} handler={(event, column) => props.handleInputs(event, i, column)}/>)}
             </tbody>
         </table>
         </>
