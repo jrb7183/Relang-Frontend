41f8da0ea28eb576bde9ba40a6ae41a1b3390f08
Josh B <jrb7183@psu.edu>
2025-08-15 17:06:18 -0400

    Implemented Feature Section Design

--- relangapp/src/pages/frontpage.js
+++ relangapp/src/pages/frontpage.js
@@ -1,9 +1,23 @@
-import "./frontpage.css"
-import NavBar from "../components/navBar"
+import "./frontpage.css";
+import NavBar from "../components/navBar";
 import { ReactComponent as Logo} from '../logo.svg';
+const textJson = require("../text.json");
 // import React, { useState } from "react";
 
+function Feature(props) {
+  return (
+    <button type="button" className="feature" onClick={props.command}>
+        <h1 className="featureName"> {props.name} </h1>
+        <p className="featureInfo"> {props.info} </p>
+    </button>
+  );
+}
+
 function Frontpage() {
+  
+  // Placeholder fŋ for actual functionality
+  const placeHolder = () => true
+
   return (
     <div className="frontpage">
       <NavBar/>
@@ -12,7 +26,17 @@ function Frontpage() {
         <Logo style={{width: "53.263%", height: "88%"}}/>
       </div>
 
-      
+      <div className="fpBody">
+        <div className="featureSection">
+          <h1 className="fpTitle"> Features </h1>
+
+          <div className="featureGrid">
+            <Feature name="Consonant Analyzer" info={textJson["Features"]["Cons"]} command={placeHolder}/>
+            <Feature name="Vowel Analyzer" info={textJson["Features"]["Vowel"]} command={placeHolder}/>
+            <Feature name="More Coming Soon!" info={textJson["Features"]["Coming Soon"]} command={placeHolder}/>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
