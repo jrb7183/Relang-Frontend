a9caaff6566b97b0a7eb4c6e9c4f8db7724e9d45
Josh B <jrb7183@psu.edu>
2025-08-15 11:32:29 -0400

    Implemented Logo

--- relangapp/src/index.js
+++ relangapp/src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-// import NavBar from './components/navBar';
+import Frontpage from './pages/frontpage';
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(<Frontpage />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- relangapp/src/pages/frontpage.js
+++ relangapp/src/pages/frontpage.js
@@ -1,9 +1,17 @@
 import "./frontpage.css"
+import NavBar from "../components/navBar"
+import { ReactComponent as Logo} from '../logo.svg';
 // import React, { useState } from "react";
 
 function Frontpage() {
   return (
     <div className="frontpage">
+      <NavBar/>
+
+      <div className="logoBackground">
+        <Logo style={{width: "53.263%", height: "88%"}}/>
+      </div>
+
       
     </div>
   )
